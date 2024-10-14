f4900c72ca8a1df66d4d24fe35c05f37d2894c64
whatkindofmilkuare <tapulele597@gmail.com>
2024-10-14 12:47:44 +0200

    home page left sie menu added

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.getElementById("sideMenu").style.width = "85%";
+    document.getElementById("mainContent").style.marginLeft = "85%";
+}
+
+function closeMenu() {
+    document.getElementById("sideMenu").style.width = "0";
+    document.getElementById("mainContent").style.marginLeft = "0";
+}
